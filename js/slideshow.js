9cadd0bcd6fadef676f8246bf7257362bd8956f5
ebremner <ebremner@uwaterloo.ca>
2019-08-22 14:16:52 -0400

    CHaning to background image for slideshow

--- js/slideshow.js
+++ js/slideshow.js
@@ -6,6 +6,34 @@
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
+        responsive: [
+          {
+            breakpoint: 1012,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 797,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
       });
     }
   };
