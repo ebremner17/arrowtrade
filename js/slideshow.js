a5e4d7d4dd4ad9c13f4c1bbc0b27e1fbbd48b026
ebremner <ebremner@uwaterloo.ca>
2019-08-28 09:30:45 -0400

    Adding full image width

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,6 +1,7 @@
 (function ($, Drupal) {
   Drupal.behaviors.slideshowBehavior = {
     attach: function (context, settings) {
+
       $('.view-at-view-cover-page-slideshow .view-content').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
